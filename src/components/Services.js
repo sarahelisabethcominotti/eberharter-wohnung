1f3454d3d799e8eb0ada1b48ac87e91329ef6612
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-11-16 15:33:10 +0000

    addition of services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { FaWifi, FaParking, FaTv, FaShower } from "react-icons/fa";
+import { MdLocalLaundryService } from "react-icons/md";
+import { GiSkis } from "react-icons/gi";
+import { RiBreadLine } from "react-icons/ri";
 
 function Services() {
   return (
     <section id="services">
-    <h2>Services</h2>
-  </section>
-)
+      <h2>Services</h2>
+      <ul className="services-list">
+        <li>
+          <FaWifi /> Wi-Fi
+        </li>
+        <li>
+          <MdLocalLaundryService /> Washer/Dryer
+        </li>
+        <li>
+          <GiSkis /> Ski and boots storage
+        </li>
+        <li>
+          <FaParking /> Parking
+        </li>
+        <li>
+          <FaTv /> Cable TV
+        </li>
+        <li>
+          <RiBreadLine /> Fresh Bread (Fee)
+        </li>
+        <li>
+          <FaShower /> Toiletries
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
